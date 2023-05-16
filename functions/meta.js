6e6e678aef63e6721fd10ab2a8ef998e353d57b2
eadbeel94 <eadbeel94@outlook.com>
2023-05-16 12:23:55 -0600

    fix: update

--- functions/meta.js
+++ functions/meta.js
@@ -1,30 +1,50 @@
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require('puppeteer-core');
 
-exports.handler = async function(event, context) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-    headless: true,
-  });
+const isDev = process.env.NODE_ENV !== 'production';
 
-  const page = await browser.newPage();
-
-  await page.goto('https://spacejelly.dev/');
-
-  const title = await page.title();
-  const description = await page.$eval('meta[name="description"]', element => element.content);
-
-  await browser.close();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      status: 'Ok',
-      page: {
-        title,
-        description
-      }
-    })
-  };
+if (isDev) {
+  require('dotenv').config()
 }
+
+exports.handler = async function(event, context) {
+  try {
+    const executablePath = process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath;
+    const browser = await puppeteer.launch({
+      executablePath,
+      headless: !isDev,
+      ignoreHTTPSErrors: true,
+      defaultViewport: chromium.defaultViewport,
+      args: chromium.args,
+    });
+  
+    const page = await browser.newPage();
+    // await page.setViewport({width: 500, height: 500});
+    await page.goto("https://dumpoir.com/v/fifthavenue", { waitUntil: "networkidle2" });
+    await page.waitForSelector("div.content__list", { timeout: 9000 });
+
+    const newList = await page.$$eval("div.content__list > div", (list) => {
+      const _list = list.map(oneItem => {
+        return oneItem.innerText;
+      });
+      return _list;
+    });
+
+    if (isDev) {
+      await page.waitForTimeout(2000);
+    }
+
+    await browser.close();
+
+    const value = { newList };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ note: 'Ok', value }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ note: error?.message, error: String(error) }),
+    };
+  };
+};
